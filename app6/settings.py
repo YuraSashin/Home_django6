4618a83cbc97f0c904808e4a119d0dd650210f49
Yura <Yura_sashin@mail.ru>
2024-02-09 17:21:22 +0300

    Решил задание 6

--- app6/settings.py
+++ app6/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-# import os
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,8 +80,15 @@ WSGI_APPLICATION = 'app6.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.mysql',
+        'NAME': 'YuraSashin$default',
+        'USER': 'YuraSashin',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'YuraSashin.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
